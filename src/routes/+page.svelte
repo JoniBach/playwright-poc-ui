f191efa76ec55126873e79b59a11daf7f8202355
james-crook <209126090+james-crook@users.noreply.github.com>
2025-11-16 12:57:04 +0000

    setting up GDS style interface

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import GovUKPage from '$lib/components/GovUKPage.svelte';
+</script>
+
+<GovUKPage title="Welcome to GOV.UK">
+	{#snippet children()}
+		<p class="govuk-body">
+			This is a SvelteKit application using the GOV.UK Design System.
+		</p>
+
+		<h2 class="govuk-heading-l">Getting started</h2>
+		<p class="govuk-body">
+			You can now use GOV.UK Frontend components in your Svelte application.
+		</p>
+
+		<button class="govuk-button" data-module="govuk-button">
+			Example button
+		</button>
+	{/snippet}
+</GovUKPage>
